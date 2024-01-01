de1633030e7a24a50c85dd7280380cd69136b55c
Amirhossein Hemmati <amirhemmati15@yahoo.com>
2024-01-01 13:49:04 +0330

    create header without logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 
 import "./App.css";
+import Header from "./component/Headers/Header";
 
 function App() {
-  return <h1>starter file</h1>
+  return <div>
+    <Header/>
+  </div>
 }
 
 export default App;
---
+++ src/component/Headers/Header.jsx
@@ -0,0 +1,43 @@
+import { MdLocationOn } from "react-icons/md";
+import { HiCalendar, HiSearch } from "react-icons/hi";
+import { useState } from "react";
+function Header() {
+  const [destination, setDestination] = useState("");
+  const [openOptions, setOpenOptions] = useState(false)
+  return (
+    <div className="header">
+      <div className="headerSearch">
+        <div className="headerSearchItem">
+          <MdLocationOn className="headerIcon locationIcon" />
+          <input
+            value={destination}
+            onChange={(e) => setDestination(e.target.value)}
+            type="text"
+            placeholder="where to go?"
+            className="headerSearchInput"
+            name="destination"
+            id="destination"
+          />
+          <span className="seperator"></span>
+        </div>
+        <div className="headerSearchItem">
+          <HiCalendar className="headerIcon dateIcon" />
+          <div className="dateDropDown">2023/06/20</div>
+          <span className="seperator"></span>
+        </div>
+        <div className="headerSearchItem">
+          <div id="optionDropDown" onClick={}>1 adult &bull; 2 children &bull; 1 room</div>
+
+          <span className="seperator"></span>
+        </div>
+        <div className="headerSearchItem">
+          <button className="headerSearchBtn">
+            <HiSearch className="headerIcon" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
